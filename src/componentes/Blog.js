816c2a977205a85d213b9ba96c3cee3b103ca715
gcancho <giovannicb19@gmail.com>
2021-01-29 17:04:47 -0500

    Cargando la lista de articulos del blog

--- src/componentes/Blog.js
+++ src/componentes/Blog.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import posts from './../data/Posts';
+import { Link } from 'react-router-dom';
+
 
 const Blog = () => {
+    console.log(posts[0].id);
     return (
         <div>
             <h2>Blog</h2>
             <ul>
-                <li>Articulo #1</li>
-                <li>Articulo #2</li>
-                <li>Articulo #3</li>
+                {posts.map((post) => {
+                    return (
+                        <li key={post.id}>
+                            <Link to={`/post/${post.id}`}>{post.titulo}</Link>
+                        </li>
+                    )
+                })}
             </ul>
         </div>
     );
